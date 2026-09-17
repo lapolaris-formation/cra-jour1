7d27b44f680427efefbafd66945afa71c46e579e
ladel1 <adel.latibi@gmail.com>
2026-09-17 22:02:34 +0200

    feat(bo): ajoute la categorie au produit

--- src/Jour1.App/bo/Produit.cs
+++ src/Jour1.App/bo/Produit.cs
@@ -3,11 +3,14 @@ class Produit
     public string Reference { get; }
     public string Nom { get; }
     public double Prix { get; }
+    public string Categorie { get; }
 
-    public Produit(string reference, string nom, double prix)
+
+    public Produit(string reference, string nom, double prix, string categorie)
     {
         Reference = reference;
         Nom = nom;
         Prix = prix;
+        Categorie = categorie;
     }
 }
