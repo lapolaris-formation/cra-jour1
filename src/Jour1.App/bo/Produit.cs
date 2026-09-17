68b053bbff3e1b173002c998dbf6722532ccd5cf
ladel1 <adel.latibi@gmail.com>
2026-09-17 22:50:36 +0200

    fix(bo): rejette un nom de produit vide

--- src/Jour1.App/bo/Produit.cs
+++ src/Jour1.App/bo/Produit.cs
@@ -6,6 +6,8 @@ class Produit
 
     public Produit(string reference, string nom, double prix)
     {
+
+        if (string.IsNullOrWhiteSpace(nom)) throw new ArgumentException("Le nom est obligatoire.");
         Reference = reference;
         Nom = nom;
         Prix = prix;
