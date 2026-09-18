59ec49696b50e644dad123cf1d27994e8b1a895c
ladel1 <adel.latibi@gmail.com>
2026-09-18 09:18:14 +0200

    fix(app): passe la reference au constructeur de Produit

--- src/Jour1.App/Program.cs
+++ src/Jour1.App/Program.cs
@@ -1,8 +1,8 @@
 ﻿
 Client client = new Client("Doe", "John", "doe.jhon@example.com");
 Console.WriteLine($"Client : {client.Prenom} {client.Nom} ({client.Email})");
-Ligne ligne1 = new Ligne(new Produit("Produit A", 10.0), 2);
-Ligne ligne2 = new Ligne(new Produit("Produit B", 20.0), 1);
+Ligne ligne1 = new Ligne(new Produit("REF-A", "Produit A", 10.0), 2);
+Ligne ligne2 = new Ligne(new Produit("REF-B", "Produit B", 20.0), 1);
 Panier panier = new Panier(client);
 panier.AjouterLigne(ligne1);
 panier.AjouterLigne(ligne2);
