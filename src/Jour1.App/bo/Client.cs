cabf04e06769b2d09be68ca530973ddbd6abda9e
ladel1 <adel.latibi@gmail.com>
2026-09-18 13:23:59 +0200

    fix(bo): precise le message d'erreur de l'email

--- src/Jour1.App/bo/Client.cs
+++ src/Jour1.App/bo/Client.cs
@@ -6,7 +6,7 @@ class Client
 
     public Client(string nom, string prenom, string email)
     {
-        if (string.IsNullOrWhiteSpace(email) || !email.Contains('@')) throw new ArgumentException("L'email est invalide.");
+        if (string.IsNullOrWhiteSpace(email) || !email.Contains('@')) throw new ArgumentException("L'adresse email du client est invalide.");
         Nom = nom;
         Prenom = prenom;
         Email = email;
